7ce55d324b3e7d2f7cf6f32452b74af7fb012992
hmarif5842 <hmarif888323@gmail.com>
2025-02-26 15:27:52 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -134,100 +134,121 @@ class HomeActivity extends StatelessWidget {
   }
 }
 
-// class Arif_Activity extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text("ABDULLAH AL RIFAT"),
-//         centerTitle: true,
-//         backgroundColor: Colors.blue,
-//       ),
-//       body: Column(
-//         children: [
-//           Row(
-//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//             children: [
-//               Container(
-//                 height: 150,
-//                 width: 150,
-//                 color: Colors.red,
-//                 child: ElevatedButton(
-//                     onPressed: () {
-//                       PopUpDialog(context, "This is Content of Red Button");
-//                     },
-//                     child: Text("Ted Button")),
-//               ),
-//               Container(
-//                 height: 150,
-//                 width: 150,
-//                 color: Colors.blue,
-//                 child: ElevatedButton(
-//                     onPressed: () {
-//                       PopUpDialog(context, "This is Content of Blue Button");
-//                     },
-//                     child: Text("Blue Button")),
-//               ),
-//               Container(
-//                 height: 150,
-//                 width: 250,
-//                 color: Colors.green,
-//                 child: Image.asset("assets/Img/rifat.jpg"),
-//               ),
-//               Container(
-//                 height: 150,
-//                 width: 150,
-//                 color: Colors.purpleAccent,
-//                 child: ElevatedButton(
-//                     onPressed: () {
-//                       PopUpDialog(context, "This is Content of Purple Button");
-//                     },
-//                     child: Text("Purple Button")),
-//               )
-//             ],
-//           ),
-//           Row(
-//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//             children: [
-//               Container(
-//                 height: 150,
-//                 width: 150,
-//                 color: Colors.purple,
-//               ),
-//               Container(
-//                 height: 150,
-//                 width: 150,
-//                 color: Colors.black,
-//                 child: ElevatedButton(
-//                     onPressed: () {
-//                       PopUpDialog(context, "This is Content of Black Button");
-//                     },
-//                     child: Text("Black Button")),
-//               ),
-//               Container(
-//                 height: 150,
-//                 width: 250,
-//                 color: Colors.green,
-//                 child: Image.asset("assets/Img/rifat.jpg"),
-//               ),
-//               Container(
-//                 height: 150,
-//                 width: 150,
-//                 color: Colors.green,
-//                 child: ElevatedButton(
-//                     onPressed: () {
-//                       PopUpDialog(context, "This is Content of Green Button");
-//                     },
-//                     child: Text("Green Button{}")),
-//               )
-//             ],
-//           ),
-//         ],
-//       ),
+class Arif_Activity extends StatelessWidget {
 
-//       // body: Center(
-//       //   child: Image.asset("assets/Img/rifat.jpg"),
-//       // ),
-//     );
-//   }
-// }
+PopUpDialog(context, contant) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext Context) {
+          return AlertDialog(
+              title: Text("Alart Box"),
+              content: Text(contant),
+              actions: [
+                TextButton(onPressed: () {}, child: Text("Yes")),
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(Context);
+                    },
+                    child: Text("No"))
+              ]);
+        });
+  }
+
+  const Arif_Activity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Arif Billah"),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                height: 150,
+                width: 150,
+                color: Colors.red,
+                child: ElevatedButton(
+                    onPressed: () {
+                      PopUpDialog(context, "This is Content of Red Button");
+                    },
+                    child: Text("Ted Button")),
+              ),
+              Container(
+                height: 150,
+                width: 150,
+                color: Colors.blue,
+                child: ElevatedButton(
+                    onPressed: () {
+                      PopUpDialog(context, "This is Content of Blue Button");
+                    },
+                    child: Text("Blue Button")),
+              ),
+              Container(
+                height: 150,
+                width: 250,
+                color: Colors.green,
+                child: Image.asset("assets/Img/rifat.jpg"),
+              ),
+              Container(
+                height: 150,
+                width: 150,
+                color: Colors.purpleAccent,
+                child: ElevatedButton(
+                    onPressed: () {
+                      PopUpDialog(context, "This is Content of Purple Button");
+                    },
+                    child: Text("Purple Button")),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                height: 150,
+                width: 150,
+                color: Colors.purple,
+              ),
+              Container(
+                height: 150,
+                width: 150,
+                color: Colors.black,
+                child: ElevatedButton(
+                    onPressed: () {
+                      PopUpDialog(context, "This is Content of Black Button");
+                    },
+                    child: Text("Black Button")),
+              ),
+              Container(
+                height: 150,
+                width: 250,
+                color: Colors.green,
+                child: Image.asset("assets/Img/rifat.jpg"),
+              ),
+              Container(
+                height: 150,
+                width: 150,
+                color: Colors.green,
+                child: ElevatedButton(
+                    onPressed: () {
+                      PopUpDialog(context, "This is Content of Green Button");
+                    },
+                    child: Text("Green Button{}")),
+              )
+            ],
+          ),
+        ],
+      ),
+
+      // body: Center(
+      //   child: Image.asset("assets/Img/rifat.jpg"),
+      // ),
+    );
+  }
+}
